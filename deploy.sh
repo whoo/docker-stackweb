c6a0d247b349bcf7391213e2f8f6d93d7b15df8e
Dominique DERRIER <derrierdo@gmail.com>
2015-10-24 22:02:04 -0400

    Comment

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-### ADD number of instance
+### ADD number of instances
 MAX=${1:-5}
 
-echo "Starting $MAX box"
+echo "Starting $MAX boxes"
 DIR="-v `pwd`/html:/var/www/html "
 
 #echo "$DIR"
@@ -19,8 +19,10 @@ CMD="$CMD --link s$a:s$a "
 LST="$LST s$a"
 done
 
+### Start Nginx with all php-fpm link
 docker run -it --rm $DIR $CMD who0/dockweb-nginx
 
+#### Remove stuff
 ####docker stop $LST
 docker rm -f $LST
 
